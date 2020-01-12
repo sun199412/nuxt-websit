19d8173612a7fdbafe4dc02abf3999fb1171ff98
dj49846917 <821084785@qq.com>
2020-01-12 23:31:39 +0800

    解决方案页面完成

---
+++ components/solutioncase/Tab.vue
@@ -0,0 +1,52 @@
+<template>
+  <ul class="tab">
+    <li v-for="item in list" :key="item.id" @click="changeTab(item)" class="tablist">
+      <span :style="item.showFlag === '1' ? activeStatus : hideStatus">{{ item.val }}</span>
+      <img :src="url" v-show="item.showFlag === '1'" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["list", "activeStatus", "hideStatus", "url"],
+  data() {
+    return {};
+  },
+  methods: {
+    changeTab(item) {
+      this.$emit('toggleTab', item)
+      // this.list.forEach((it, index) => {
+      //   if (it.id === item.id) {
+      //     this.$set(this.list[index], "showFlag", "1");
+      //   } else {
+      //     this.$set(this.list[index], "showFlag", "2");
+      //   }
+      // });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$titleColor: #1c1e2b;
+$activeColor: #fc6b00;
+.tab {
+  color: $titleColor;
+  font-size: 18px;
+  margin-right: 88px;
+  .tablist {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 144px;
+    border-top: 0.5px solid #e8e5e5;
+    border-bottom: 0.5px solid #e8e5e5;
+    padding: 10px 0;
+    cursor: pointer;
+    &:hover {
+      color: #fc6b00;
+    }
+  }
+}
+</style>
--- pages/SolutionCase.vue
+++ pages/SolutionCase.vue
@@ -1,25 +1,22 @@
 <template>
   <div class="solution">
     <img :src="bannerUrl" class="box_img" />
-    <div>
-      <el-tabs tab-position="left" style="height: 200px;">
-        <el-tab-pane label="用户管理">用户管理</el-tab-pane>
-        <el-tab-pane label="配置管理">配置管理</el-tab-pane>
-        <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-        <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
-      </el-tabs>
-    </div>
+    <case></case>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        bannerUrl: '/images/pic_jjfa_banner.png'
-      }
-    },
-  }
+import Case from "~/components/solutioncase/Case";
+export default {
+  components: {
+    Case
+  },
+  data() {
+    return {
+      bannerUrl: "/images/pic_jjfa_banner.png",
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -27,11 +24,5 @@
   .solution_img {
     width: 100%;
   }
-}
-/deep/ .el-tabs__item {
-  height: 45px;
-  line-height: 45px;
-  border-top: 0.5px solid #E8E5E5;
-  border-bottom: 0.5px solid #E8E5E5;
 }
 </style>
